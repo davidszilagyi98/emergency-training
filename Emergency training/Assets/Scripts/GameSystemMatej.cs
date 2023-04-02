448ba87b8b368e7ddb38c9220ddc1a023ab05535
Matej Benes <eaamateb@students.eaaa.dk>
2023-04-02 17:30:24 +0200

    Scripts working - UI needs to be added

--- Emergency training/Assets/Scripts/GameSystemMatej.cs
+++ Emergency training/Assets/Scripts/GameSystemMatej.cs
@@ -7,18 +7,60 @@ public class GameSystemMatej : MonoBehaviour
 {
     public TextMeshProUGUI titleText;
     public TextMeshProUGUI descriptionText;
-    private int currentStepIndex = 0;
-    private void Update()
+    public int currentStepIndex = 0;
+
+    public static GameSystemMatej Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        UpdateUI();
+    }
+  /*  private void Update()
     {
         if (Input.GetKeyUp(KeyCode.P))
         {
             EscapeStep myStep = GameManagerMatej.Instance.GetSteps(currentStepIndex);
             titleText.text = myStep.title.ToString();
             descriptionText.text = myStep.description.ToString();
-            currentStepIndex++;
+
+            // Only increment currentStepIndex if the current step is marked as done
+            if (myStep.isDone)
+            {
+                currentStepIndex++;
+                if (currentStepIndex >= GameManagerMatej.Instance.steps.Length)
+                {
+                    currentStepIndex = 0;
+                }
+                {
+                    UpdateUI();
+                }
+            }
+
+            //currentStepIndex++;
             //currentStepIndex = (currentStepIndex + 1) % GameManagerMatej.Instance.steps.Length;
             //Debug.Log(myStep.title+ " " + myStep.description);
         }
-    }
+    } */
 
+    public void UpdateUI()
+    {
+        EscapeStep myStep = GameManagerMatej.Instance.GetSteps(currentStepIndex);
+        titleText.text = myStep.title.ToString();
+        descriptionText.text = myStep.description.ToString();
+        // Only increment currentStepIndex if the current step is marked as done
+        if (myStep.isDone)
+        {
+            //currentStepIndex++;
+            if (currentStepIndex >= GameManagerMatej.Instance.steps.Length)
+            {
+                currentStepIndex = 0;
+            }
+
+        }
+    }
 }
---
+++ Emergency training/Assets/Scripts/StepTrigger.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class StepTrigger : MonoBehaviour
+{
+
+    public bool isTriggered = false;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isTriggered = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isTriggered = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (isTriggered && Input.GetKeyDown(KeyCode.E))
+        {
+            EscapeStep currentStep = GameManagerMatej.Instance.GetSteps(GameSystemMatej.Instance.currentStepIndex);
+            if (currentStep.isDone)
+            {
+                GameSystemMatej.Instance.currentStepIndex++;
+                if (GameSystemMatej.Instance.currentStepIndex >= GameManagerMatej.Instance.steps.Length)
+                {
+                    GameSystemMatej.Instance.currentStepIndex = 0;
+                }
+                GameSystemMatej.Instance.UpdateUI();
+            }
+        }
+    }
+}
